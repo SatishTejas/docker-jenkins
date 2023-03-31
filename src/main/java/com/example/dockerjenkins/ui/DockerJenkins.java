e5aef136c4e2529859dac23d11dd2ec63a5204b8
Satish <tejassatish@hotmail.com>
2023-03-31 19:14:05 +0530

    Docker-Jenkins First Commit .

---
+++ src/main/java/com/example/dockerjenkins/ui/DockerJenkins.java
@@ -0,0 +1,16 @@
+package com.example.dockerjenkins.ui;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/docker-jenkins")
+public class DockerJenkins {
+
+	@GetMapping
+	public String find() {
+
+		return "Docker With Jenkins";
+	}
+}
